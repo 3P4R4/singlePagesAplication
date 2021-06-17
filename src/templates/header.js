6ff56cb403822441152965c8642e0a4ebeeeafcf
lenin jose mendoza espina <mendoza124302@gmail.com>
2021-06-17 15:17:05 -0500

    cambiando el header

--- src/templates/header.js
+++ src/templates/header.js
@@ -4,7 +4,7 @@ const header = () => {
             <div class="header-logo">
                 <h1>
                     <a href="/">
-                        100ti.co
+                        API rickAndMorty por - leninMendoza
                     </a>
                 </h1>
             </div>
